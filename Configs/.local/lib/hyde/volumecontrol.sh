bfad2851369efa5bb61ecba53c1ba3ddbdd72379
kRHYME7 <alampayk@gmail.com>
2024-11-28 15:54:11 +0800

    fix volume

--- Configs/.local/lib/hyde/volumecontrol.sh
+++ Configs/.local/lib/hyde/volumecontrol.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Source global control script
 scrDir=$(dirname "$(realpath "$0")")
+# shellcheck disable=SC1091
 source "$scrDir/globalcontrol.sh"
+confDir=${confDir:-$XDG_CONFIG_HOME}
 
 # Check if SwayOSD is installed
 use_swayosd=false
@@ -42,20 +44,20 @@ EOF
 }
 
 notify_vol() {
-    angle=$(( (($vol + 2) / 5) * 5 ))
+    angle=$((((vol + 2) / 5) * 5))
     iconStyle="knob"
     ico="${icodir}/${iconStyle}-${angle}.svg"
-    bar=$(seq -s "." $(($vol / 15)) | sed 's/[0-9]//g')
-    notify-send -a "t2" -r 91190 -t 800 -i "${ico}" "${vol}${bar}" "${nsink}"
+    bar=$(seq -s "." $((vol / 15)) | sed 's/[0-9]//g')
+    notify-send -a "t2" -r 69 -t 800 -i "${ico}" "${vol}${bar}" "${nsink}"
 }
 
 notify_mute() {
     mute=$(pamixer "${srce}" --get-mute | cat)
     [ "${srce}" == "--default-source" ] && dvce="mic" || dvce="speaker"
     if [ "${mute}" == "true" ]; then
-        notify-send -a "t2" -r 91190 -t 800 -i "${icodir}/muted-${dvce}.svg" "muted" "${nsink}"
+        notify-send -a "t2" -r 69 -t 800 -i "${icodir}/muted-${dvce}.svg" "muted" "${nsink}"
     else
-        notify-send -a "t2" -r 91190 -t 800 -i "${icodir}/unmuted-${dvce}.svg" "unmuted" "${nsink}"
+        notify-send -a "t2" -r 69 -t 800 -i "${icodir}/unmuted-${dvce}.svg" "unmuted" "${nsink}"
     fi
 }
 
@@ -69,15 +71,15 @@ change_volume() {
     [ "${action}" = "i" ] && delta="+"
     [ "${srce}" = "--default-source" ] && mode="--input-volume"
     case $device in
-        "pamixer")
-            $use_swayosd && swayosd-client ${mode} "${delta}${step}"  && exit 0
-            pamixer $srce -"$action" "$step"
-            vol=$(pamixer $srce --get-volume)
-            ;;
-        "playerctl")
-            playerctl --player="$srce" volume "$(awk -v step="$step" 'BEGIN {print step/100}')${delta}"
-            vol=$(playerctl --player="$srce" volume | awk '{ printf "%.0f\n", $0 * 100 }')
-            ;;
+    "pamixer")
+        $use_swayosd && swayosd-client ${mode} "${delta}${step}" && exit 0
+        pamixer "$srce" -"$action" "$step"
+        vol=$(pamixer "$srce" --get-volume)
+        ;;
+    "playerctl")
+        playerctl --player="$srce" volume "$(awk -v step="$step" 'BEGIN {print step/100}')${delta}"
+        vol=$(playerctl --player="$srce" volume | awk '{ printf "%.0f\n", $0 * 100 }')
+        ;;
     esac
 
     notify_vol
@@ -88,26 +90,27 @@ toggle_mute() {
     local mode="--output-volume"
     [ "${srce}" = "--default-source" ] && mode="--input-volume"
     case $device in
-        "pamixer")
-            $use_swayosd && swayosd-client "${mode}" mute-toggle && exit 0
-            pamixer $srce -t
-            notify_mute
-            ;;
-        "playerctl")
-            local volume_file="/tmp/$(basename "$0")_last_volume_${srce:-all}"
-            if [ "$(playerctl --player="$srce" volume | awk '{ printf "%.2f", $0 }')" != "0.00" ]; then
-                playerctl --player="$srce" volume | awk '{ printf "%.2f", $0 }' > "$volume_file"
-                playerctl --player="$srce" volume 0
+    "pamixer")
+        $use_swayosd && swayosd-client "${mode}" mute-toggle && exit 0
+        pamixer "$srce" -t
+        notify_mute
+        ;;
+    "playerctl")
+        local volume_file
+        volume_file="/tmp/$(basename "$0")_last_volume_${srce:-all}"
+        if [ "$(playerctl --player="$srce" volume | awk '{ printf "%.2f", $0 }')" != "0.00" ]; then
+            playerctl --player="$srce" volume | awk '{ printf "%.2f", $0 }' >"$volume_file"
+            playerctl --player="$srce" volume 0
+        else
+            if [ -f "$volume_file" ]; then
+                last_volume=$(cat "$volume_file")
+                playerctl --player="$srce" volume "$last_volume"
             else
-                if [ -f "$volume_file" ]; then
-                    last_volume=$(cat "$volume_file")
-                    playerctl --player="$srce" volume "$last_volume"
-                else
-                    playerctl --player="$srce" volume 0.5  # Default to 50% if no saved volume
-                fi
+                playerctl --player="$srce" volume 0.5 # Default to 50% if no saved volume
             fi
-            notify_mute
-            ;;
+        fi
+        notify_mute
+        ;;
     esac
 }
 
@@ -126,39 +129,61 @@ select_output() {
 }
 
 toggle_output() {
-    local default_sink=$(pamixer --get-default-sink | awk -F '"' 'END{print $(NF - 1)}')
+    local default_sink
+    local current_index
+    default_sink=$(pamixer --get-default-sink | awk -F '"' 'END{print $(NF - 1)}')
     mapfile -t sink_array < <(select_output)
-    local current_index=$(printf '%s\n' "${sink_array[@]}" | grep -n "$default_sink" | cut -d: -f1)
-    local next_index=$(( (current_index % ${#sink_array[@]}) + 1 ))
-    local next_sink="${sink_array[next_index-1]}"
+    current_index=$(printf '%s\n' "${sink_array[@]}" | grep -n "$default_sink" | cut -d: -f1)
+    local next_index=$(((current_index % ${#sink_array[@]}) + 1))
+    local next_sink="${sink_array[next_index - 1]}"
     select_output "$next_sink"
 }
 
 # Main script logic
 
 # Set default variables
+iconsDir="${iconsDir:-$XDG_DATA_HOME/icons}"
 icodir="${iconsDir}/Wallbash-Icon/media"
 step=5
 # Parse options
 while getopts "iop:st" opt; do
     case $opt in
-        i) device="pamixer"; srce="--default-source"; nsink=$(pamixer --list-sources | awk -F '"' 'END {print $(NF - 1)}') ;;
-        o) device="pamixer"; srce=""; nsink=$(pamixer --get-default-sink | awk -F '"' 'END{print $(NF - 1)}') ;;
-        p) device="playerctl"; srce="${OPTARG}"; nsink=$(playerctl --list-all | grep -w "$srce") ;;
-        s) select_output "$(select_output | rofi -dmenu -config "${confDir}/rofi/notification.rasi")"; exit ;;
-        t) toggle_output; exit ;;
-        *) print_usage ;;
+    i)
+        device="pamixer"
+        srce="--default-source"
+        nsink=$(pamixer --list-sources | awk -F '"' 'END {print $(NF - 1)}')
+        ;;
+    o)
+        device="pamixer"
+        srce=""
+        nsink=$(pamixer --get-default-sink | awk -F '"' 'END{print $(NF - 1)}')
+        ;;
+    p)
+        device="playerctl"
+        srce="${OPTARG}"
+        nsink=$(playerctl --list-all | grep -w "$srce")
+        ;;
+    s)
+
+        select_output "$(select_output | rofi -dmenu -config "${confDir}/rofi/notification.rasi")"
+        exit
+        ;;
+    t)
+        toggle_output
+        exit
+        ;;
+    *) print_usage ;;
     esac
 done
 
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
 # Check if device is set
 [ -z "$device" ] && print_usage
 
 # Execute action
 case $1 in
-    i|d) change_volume "$1" "${2:-$step}" "$device" ;;
-    m) toggle_mute "$device" ;;
-    *) print_usage ;;
+i | d) change_volume "$1" "${2:-$step}" "$device" ;;
+m) toggle_mute "$device" ;;
+*) print_usage ;;
 esac
