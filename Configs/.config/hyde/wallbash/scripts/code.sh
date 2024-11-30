f0f6938c484de83fe523a826e8c1f719f3606162
kRHYME7 <alampayk@gmail.com>
2024-11-30 10:08:08 +0800

    code installer

--- Configs/.config/hyde/wallbash/scripts/code.sh
+++ Configs/.config/hyde/wallbash/scripts/code.sh
@@ -1,16 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
+scrDir="$(dirname "$(which globalcontrol.sh)")"
+# shellcheck disable=SC1091
+source "${scrDir}/globalcontrol.sh"
 
-readarray -t codeConf < <(find "${confDir}" -mindepth 1 -maxdepth 1 -type d -name "Code*" -o -name "VSCodium*" -o -name "Cursor*" | sort)
+cacheDir="${cacheDir:-$XDG_CACHE_HOME/hyde}"
+# confDir="${confDir:-$XDG_CONFIG_HOME}"
+# readarray -t codeConf < <(find "$confDir" -mindepth 1 -maxdepth 1 -type d -name "Code*" -o -name "VSCodium*" -o -name "Cursor*" | sort)
 readarray -t codeVsix < <(find "$HOME" -mindepth 1 -maxdepth 1 -type d -name ".vscode*" -o -name ".cursor" | sort)
-tmpFile="/tmp/$(id -u)$(basename ${0}).tmp"
-tgtFile="extensions/undefined_publisher.wallbash-0.0.1/themes/wallbash-color-theme.json"
+# tmpFile="/tmp/$(id -u)$(basename ${0}).tmp"
 
 #// install  ext
-
-for i in "${!codeVsix[@]}" ;do
-    if [ ! -f "${codeVsix[i]}/${tgtFile}" ] ; then
-        [ -f "${cacheDir}/landing/Code_Wallbash.vsix" ] || curl -L -o "${cacheDir}/landing/Code_Wallbash.vsix" https://github.com/prasanthrangan/hyprdots/raw/main/Source/arcs/Code_Wallbash.vsix
+for i in "${!codeVsix[@]}"; do
+    [[ -z "${codeVsix[i]}" ]] && continue
+    file=$(find "${codeVsix[i]}" -type f -path "*extensions/thehydeproject*" -name "wallbash.json")
+    if [ -z "${file}" ]; then
+        [ -f "${cacheDir}/landing/Code_Wallbash.vsix" ] || curl -L -o "${cacheDir}/landing/Code_Wallbash.vsix" https://github.com/HyDE-Project/code-wallbash/raw/refs/heads/master/release/Code_Wallbash.vsix
         case ${codeVsix[i]} in
         *".cursor"*)
             echo "[wallbashcode] Cursor IDE: Manual intervention required for extension installation."
@@ -20,20 +25,25 @@ for i in "${!codeVsix[@]}" ;do
             pkg_installed code-insiders && code-insiders --install-extension "${cacheDir}/landing/Code_Wallbash.vsix"
             pkg_installed code && code --install-extension "${cacheDir}/landing/Code_Wallbash.vsix"
             pkg_installed vscodium && vscodium --install-extension "${cacheDir}/landing/Code_Wallbash.vsix"
+            echo "[wallbashcode] Installed: in ${codeVsix[i]}"
             ;;
         esac
     fi
-    [ -d "$(dirname "${codeVsix[i]}/${tgtFile}")" ] && cp "${cacheDir}/landing/wallbashcode.json" "${codeVsix[i]}/${tgtFile}"
 done
 
 #// apply theme
 
-for i in "${!codeConf[@]}" ; do
-    [ -d "${codeConf[i]}/User" ] || continue
-    [ -f "${codeConf[i]}/User/settings.json" ] ||  echo -e "{\n \"workbench.colorTheme\":\"wallbash\" \n}" > "${codeConf[i]}/User/settings.json"
-    extTheme="$(jq -r '.["workbench.colorTheme"]' "${codeConf[i]}/User/settings.json")"
+cat <<NOTICE
+[wallbashcode] To apply the Wallbash theme, open the command palette (Ctrl+Shift+P)
+[wallbashcode]  and type "Preferences: Color Theme" and select "Wallbash".
+NOTICE
 
-    if [ "${extTheme}" != "wallbash" ] ; then
-        jq '.["workbench.colorTheme"] = "wallbash"' "${codeConf[i]}/User/settings.json" > "${tmpFile}" && mv "${tmpFile}" "${codeConf[i]}/User/settings.json"
-    fi
-done
+# for i in "${!codeConf[@]}"; do
+#     [ -d "${codeConf[i]}/User" ] || continue
+#     [ -f "${codeConf[i]}/User/settings.json" ] || echo -e "{\n \"workbench.colorTheme\":\"Wallbash\" \n}" >"${codeConf[i]}/User/settings.json"
+#     extTheme="$(jq -r '.["workbench.colorTheme"]' "${codeConf[i]}/User/settings.json")"
+
+#     if [ "${extTheme}" != "Wallbash" ]; then
+#         jq '.["workbench.colorTheme"] = "Wallbash"' "${codeConf[i]}/User/settings.json" >"${tmpFile}" && mv "${tmpFile}" "${codeConf[i]}/User/settings.json"
+#     fi
+# done
