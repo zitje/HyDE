5cf4cfffe28bdd9e9d837fba9d7006b38850d92e
kRHYME7 <alampayk@gmail.com>
2024-12-08 15:16:08 +0800

    spotify support  custom spotify_path

--- Configs/.config/hyde/wallbash/scripts/spotify.sh
+++ Configs/.config/hyde/wallbash/scripts/spotify.sh
@@ -1,24 +1,42 @@
 #!/usr/bin/env bash
 
 # regen conf
-if pkg_installed spotify && pkg_installed spicetify-cli ; then
-cacheDir="${cacheDir:-$XDG_CACHE_HOME/hyde}"
+if (pkg_installed spotify && pkg_installed spicetify-cli) || [ -n "$SPOTIFY_PATH" ]; then
+    cacheDir="${cacheDir:-$XDG_CACHE_HOME/hyde}"
+    shareDir=${XDG_DATA_HOME:-$HOME/.local/share}
 
-    if [ ! -w /opt/spotify ] || [ ! -w /opt/spotify/Apps ]; then
-        notify-send -a "Hyprdots" "Permission needed for Wallbash Spotify theme"
-        pkexec chmod a+wr /opt/spotify
-        pkexec chmod a+wr /opt/spotify/Apps -R
+    if [ -n "${SPOTIFY_PATH}" ]; then
+        spotify_path="${SPOTIFY_PATH}"
+        cat <<EOF
+[warning]   using custom spotify path
+            ensure to have proper permissions for ${SPOTIFY_PATH}
+            run:
+            chmod a+wr ${SPOTIFY_PATH}
+            chmod a+wr -R ${SPOTIFY_PATH}/Apps
+
+            note: run with 'sudo' if only needed.
+EOF
+    elif [ -f "${shareDir}/spotify-launcher/install/usr/bin/spotify" ]; then
+        spotify_path="${shareDir}/spotify-launcher/install/usr/bin/spotify" # default for spotify-launcher
+    elif [ -d /opt/spotify ]; then
+        spotify_path='/opt/spotify' # default for AUR
+        if [ ! -w "${spotify_path}" ] || [ ! -w "${spotify_path}/Apps" ]; then
+            # For spotify in /opt/spotify \\ AUR
+            notify-send -a "HyDE Alert" "Permission needed for Wallbash Spotify theme"
+            pkexec chmod a+wr ${spotify_path}
+            pkexec chmod a+wr -R ${spotify_path}/Apps
+        fi
     fi
-
-    if [ "$(spicetify config | awk '{if ($1=="color_scheme") print $2}')" != "Wallbash" ] || [[ "${*}" == *"--reset"*  ]] ; then
-        spicetify &> /dev/null
+    if [ "$(spicetify config | awk '{if ($1=="color_scheme") print $2}')" != "Wallbash" ] || [[ "${*}" == *"--reset"* ]]; then
+        spicetify &>/dev/null
         mkdir -p ~/.config/spotify
         touch ~/.config/spotify/prefs
-        sptfyConf=$(spicetify -c)
-        spotfy_flags='--ozone-platform=wayland'
+        spotifyConf=$(spicetify -c)
+        spotify_flags='--ozone-platform=wayland'
         sed -i -e "/^prefs_path/ s+=.*$+= $HOME/.config/spotify/prefs+g" \
-            -e "/^spotify_launch_flags/ s+=.*$+= $spotfy_flags+g" "$sptfyConf"
-	    curl -L -o "${cacheDir}/landing/Spotify_Sleek.tar.gz" "https://github.com/prasanthrangan/hyprdots/raw/main/Source/arcs/Spotify_Sleek.tar.gz"
+            -e "/^spotify_path/ s+=.*$+= $spotify_path+g" \
+            -e "/^spotify_launch_flags/ s+=.*$+= $spotify_flags+g" "$spotifyConf"
+        curl -L -o "${cacheDir}/landing/Spotify_Sleek.tar.gz" "https://github.com/prasanthrangan/hyprdots/raw/main/Source/arcs/Spotify_Sleek.tar.gz"
         tar -xzf "${cacheDir}/landing/Spotify_Sleek.tar.gz" -C ~/.config/spicetify/Themes/
         spicetify backup apply
         spicetify config current_theme Sleek
@@ -26,10 +44,9 @@ cacheDir="${cacheDir:-$XDG_CACHE_HOME/hyde}"
         spicetify apply
     fi
 
-    if pgrep -x spotify > /dev/null ; then
+    if pgrep -x spotify >/dev/null; then
         pkill -x spicetify
         spicetify -q watch -s &
     fi
 
 fi
-
