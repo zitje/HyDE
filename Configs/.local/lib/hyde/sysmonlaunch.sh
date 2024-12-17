76a6f877d7c89fce6f9868bd0198aa9b6cb9b7a6
kRHYME7 <alampayk@gmail.com>
2024-12-17 11:51:50 +0800

    feat: use proper variable and config for sysmonitor.

--- Configs/.local/lib/hyde/sysmonlaunch.sh
+++ Configs/.local/lib/hyde/sysmonlaunch.sh
@@ -1,14 +1,29 @@
-#!/usr/bin/env sh
+#!/bin/env bash
 
 scrDir="$(dirname "$(realpath "$0")")"
+# shellcheck disable=SC1091
 source "${scrDir}/globalcontrol.sh"
 pkgChk=("io.missioncenter.MissionCenter" "htop" "btop" "top")
+pkgChk+=("${SYSMONITOR_COMMANDS[@]}")
+
+executable="${SYSMONITOR_EXECUTE:-${pkgChk[sysMon]}}"
+
+exec_cmd() {
+
+    if pkg_installed "${executable}"; then
+        if command -v "$term" >/dev/null; then
+            hyprctl dispatch exec "[ float ] ${term} ${executable}" &
+        else
+            hyprctl dispatch exec "[ float ] ${executable}" &
+        fi
+    fi
+
+}
 
 for sysMon in "${!pkgChk[@]}"; do
-    [ "${sysMon}" -gt 0 ] && term=$(cat $HOME/.config/hypr/keybindings.conf | grep ^'$TERMINAL' | cut -d '=' -f2)
-    if pkg_installed "${pkgChk[sysMon]}" ; then
-        pkill -x "${pkgChk[sysMon]}" || ${term} "${pkgChk[sysMon]}" &
-        break
-    fi
+    term=$(grep -E '^\s*'"$term" "$HOME/.config/hypr/keybindings.conf" | cut -d '=' -f2 | xargs) # dumb search the config
+    term=${TERMINAL:-$term}                                                                      # Use env var
+    term=${SYSMONITOR_TERMINAL:-$term}                                                           # use the declared variable
+    pkill -x "${executable}" || exec_cmd "${executable}"
+    break
 done
-
