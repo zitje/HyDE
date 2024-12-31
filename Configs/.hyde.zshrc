032e2114a69cba21ddb3daf6dfa6d85f7c43e516
Khing <53417443+kRHYME7@users.noreply.github.com>
2024-12-31 20:45:00 +0800

    breaking (zshrc): Rework for the zshrc implementation (#72)
    * feat: Won't .zshrc
    feat: easy updates for hyde
    feat: User configuration
    feat: can uninstall hyde without removing users zshrc
    feat: put oh-my-zsh locally
    feat: update omz on restore_shl.sh
    
    * fix: prevent omz  overriding eza as alias
    feat: eza can also be removed and alias should still work!
    
    * fix (exa -> eza)  : bad habit of exa lol
    
    * path
    
    * path

---
+++ Configs/.hyde.zshrc
@@ -0,0 +1,46 @@
+#  Startup 
+# Commands on startup (before the prompt is shown)
+# This is a good place to load graphic/ascii art, display system information, etc.
+
+# pokego --no-title -r 1,3,6
+# fastfetch --logo-type kitty
+# fastfetch.sh
+
+#  Aliases 
+# Override aliases here or in '~/.zshrc' (already set in .zshenv)
+
+# # Helpful aliases
+# alias c='clear'                                                        # clear terminal
+# alias l='eza -lh --icons=auto'                                         # long list
+# alias ls='eza -1 --icons=auto'                                         # short list
+# alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+# alias ld='eza -lhD --icons=auto'                                       # long list dirs
+# alias lt='eza --icons=auto --tree'                                     # list folder as tree
+# alias un='$aurhelper -Rns'                                             # uninstall package
+# alias up='$aurhelper -Syu'                                             # update system/package/aur
+# alias pl='$aurhelper -Qs'                                              # list installed package
+# alias pa='$aurhelper -Ss'                                              # list available package
+# alias pc='$aurhelper -Sc'                                              # remove unused cache
+# alias po='$aurhelper -Qtdq | $aurhelper -Rns -'                        # remove unused packages, also try > $aurhelper -Qqd | $aurhelper -Rsu --print -
+# alias vc='code'                                                        # gui code editor
+# alias fastfetch='fastfetch --logo-type kitty'
+
+# # Directory navigation shortcuts
+# alias ..='cd ..'
+# alias ...='cd ../..'
+# alias .3='cd ../../..'
+# alias .4='cd ../../../..'
+# alias .5='cd ../../../../..'
+
+# # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+# alias mkdir='mkdir -p'
+
+#  Plugins 
+# manually add your oh-my-zsh plugins here
+plugins=(
+    "sudo"
+    # "git"                     # (default)
+    # "zsh-autosuggestions"     # (default)
+    # "zsh-syntax-highlighting" # (default)
+    # "zsh-completions"         # (default)
+)
--- Scripts/restore_shl.sh
+++ Scripts/restore_shl.sh
@@ -13,32 +13,51 @@ fi
 
 # shellcheck disable=SC2154
 if chk_list "myShell" "${shlList[@]}"; then
-    print_log -g "[SHELL] " -b "detected :: " "${myShell}"
+    print_log -sec "SHELL" -stat "detected" "${myShell}"
 else
-    print_log -g "[SHELL] " -r "error :: " "no shell found..."
+    print_log -sec "SHELL" -err "error" "no shell found..."
     exit 1
 fi
 
 # add zsh plugins
-# TODO Nuke this and try to remove oh-my-zsh
 if pkg_installed zsh; then
 
-    #? Optional: oh-my-zsh 
-    if pkg_installed oh-my-zsh-git; then
+    if ! pkg_installed oh-my-zsh-git; then
+        if [[ ! -e "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]]; then
+            print_log -sec "SHELL" -stat "cloning" "oh-my-zsh"
+            sh -c "$(curl -fsSL https://install.ohmyz.sh/)" "" --unattended --keep-zshrc
+        else
+            print_log -sec "SHELL" -stat "updating" "oh-my-zsh"
+            zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/upgrade.sh)"
+        fi
+    fi
+
+    #? Optional: oh-my-zsh
+    if pkg_installed oh-my-zsh-git || [[ -f "${HOME}/.oh-my-zsh/oh-my-zsh.sh" ]]; then
+        zsh_paths=(
+            "$HOME/.oh-my-zsh"
+            "/usr/local/share/oh-my-zsh"
+            "/usr/share/oh-my-zsh"
+        )
+        for zsh_path in "${zsh_paths[@]}"; do [[ -d $zsh_path ]] && Zsh_Path=$zsh_path && break; done
 
         # set variables
-        Zsh_rc="${ZDOTDIR:-$HOME}/.zshrc"
-        Zsh_Path="/usr/share/oh-my-zsh"
+        Zsh_rc="${ZDOTDIR:-$HOME}/.zshenv"
+        Zsh_Path="${Zsh_Path:-$HOME/.oh-my-zsh}"
         Zsh_Plugins="$Zsh_Path/custom/plugins"
         Fix_Completion=""
 
         # generate plugins from list
         while read -r r_plugin; do
-            z_plugin=$(awk -F '/' '{print $NF}' <<< "${r_plugin}")
+            z_plugin=$(awk -F '/' '{print $NF}' <<<"${r_plugin}")
             if [ "${r_plugin:0:4}" == "http" ] && [ ! -d "${Zsh_Plugins}/${z_plugin}" ]; then
-                sudo git clone "${r_plugin}" "${Zsh_Plugins}/${z_plugin}"
+                if [ -w "${Zsh_Plugins}" ]; then
+                    git clone "${r_plugin}" "${Zsh_Plugins}/${z_plugin}"
+                else
+                    sudo git clone "${r_plugin}" "${Zsh_Plugins}/${z_plugin}"
+                fi
             fi
-            if [ "${z_plugin}" == "zsh-completions" ] && [ "$(grep 'fpath+=.*plugins/zsh-completions/src' "${Zsh_rc}" | wc -l)" -eq 0 ]; then
+            if [ "${z_plugin}" == "zsh-completions" ] && [ "$(grep -c 'fpath+=.*plugins/zsh-completions/src' "${Zsh_rc}")" -eq 0 ]; then
                 Fix_Completion='\nfpath+=${ZSH_CUSTOM:-${ZSH:-/usr/share/oh-my-zsh}/custom}/plugins/zsh-completions/src'
             else
                 [ -z "${z_plugin}" ] || w_plugin+=" ${z_plugin}"
@@ -46,15 +65,15 @@ if pkg_installed zsh; then
         done < <(cut -d '#' -f 1 "${scrDir}/restore_zsh.lst" | sed 's/ //g')
 
         # update plugin array in zshrc
-        print_log -g "[SHELL] " -b "installing :: " "plugins (${w_plugin} )"
-        sed -i "/^plugins=/c\plugins=(${w_plugin} )${Fix_Completion}" "${Zsh_rc}"
+        print_log -sec "SHELL" -stat "installing" "plugins (${w_plugin} )"
+        sed -i "/^hyde_plugins=/c\hyde_plugins=(${w_plugin} )${Fix_Completion}" "${Zsh_rc}"
     fi
 fi
 
 # set shell
 if [[ "$(grep "/${USER}:" /etc/passwd | awk -F '/' '{print $NF}')" != "${myShell}" ]]; then
-    print_log -g "[SHELL] " -b "change :: " "shell to ${myShell}..."
+    print_log -sec "SHELL" -stat "change" "shell to ${myShell}..."
     chsh -s "$(which "${myShell}")"
 else
-    print_log -y "[SHELL] " -y "exist :: " "${myShell} is already set as shell..."
+    print_log -sec "SHELL" -stat "exist" "${myShell} is already set as shell..."
 fi
