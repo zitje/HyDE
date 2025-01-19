6297c41b89482b98e53715b4bcfcf820728e4753
kRHYME7 <bong.kook.khan@gmail.com>
2025-01-20 02:32:40 +0800

    fix (sysmonlaunch)
    fixes this
    
    https://discord.com/channels/1200448076620501063/1330556988962242600
    
    ❯ sysmonlaunch.sh
    pkill: pattern that searches for process name longer than 15 characters will result in zero matches
    Try `pkill -f' option to match against the complete command line.

--- Configs/.local/lib/hyde/sysmonlaunch.sh
+++ Configs/.local/lib/hyde/sysmonlaunch.sh
@@ -6,24 +6,15 @@ source "${scrDir}/globalcontrol.sh"
 pkgChk=("io.missioncenter.MissionCenter" "htop" "btop" "top")
 pkgChk+=("${SYSMONITOR_COMMANDS[@]}")
 
-executable="${SYSMONITOR_EXECUTE:-${pkgChk[sysMon]}}"
-
-exec_cmd() {
-
-    if pkg_installed "${executable}"; then
-        if command -v "$term" >/dev/null; then
-            hyprctl dispatch exec "[ float ] ${term} ${executable}" &
-        else
-            hyprctl dispatch exec "[ float ] ${executable}" &
-        fi
+for sysMon in "${!pkgChk[@]}"; do
+    if [ "${sysMon}" -gt 0 ]; then
+        term=$(grep -E '^\s*'"$term" "$HOME/.config/hypr/keybindings.conf" | cut -d '=' -f2 | xargs) # dumb search the config
+        term=${TERMINAL:-$term}                                                                      # Use env var
+        term=${SYSMONITOR_TERMINAL:-$term}                                                           # use the declared variable
     fi
 
-}
-
-for sysMon in "${!pkgChk[@]}"; do
-    term=$(grep -E '^\s*'"$term" "$HOME/.config/hypr/keybindings.conf" | cut -d '=' -f2 | xargs) # dumb search the config
-    term=${TERMINAL:-$term}                                                                      # Use env var
-    term=${SYSMONITOR_TERMINAL:-$term}                                                           # use the declared variable
-    pkill -x "${executable}" || exec_cmd "${executable}"
-    break
+    if pkg_installed "${pkgChk[sysMon]}"; then
+        pkill -x "${pkgChk[sysMon]}" || ${term} "${pkgChk[sysMon]}" &
+        break
+    fi
 done
