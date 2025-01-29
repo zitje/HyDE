39ad97f74c4653c6f6d760a41073ab3ab5f6ec26
kRHYME7 <bong.kook.khan@gmail.com>
2025-01-29 09:29:47 +0800

    waybar uses global font

--- Configs/.local/lib/hyde/wbarstylegen.sh
+++ Configs/.local/lib/hyde/wbarstylegen.sh
@@ -104,9 +104,9 @@ left | right)
     ;;
 esac
 
-theme_font="$(get_hyprConf WAYBAR_FONT)"
-WAYBAR_FONT=${theme_font:-$WAYBAR_FONT}
-export font_name=${WAYBAR_FONT:-"JetBrainsMono Nerd Font"}
+font_name=${WAYBAR_FONT:-$(get_hyprConf "WAYBAR_FONT")}
+font_name=${font_name:-$(get_hyprConf "FONT")}
+export font_name=${font_name:-"JetBrainsMono Nerd Font"}
 
 # list modules and generate theme style
 export modules_ls
