670c769a5fefd37567d537c6e000670390ca1a87
kRHYME7 <bong.kook.khan@gmail.com>
2025-01-29 23:32:52 +0800

    chore: -q

--- Configs/.local/lib/hyde/themeswitch.sh
+++ Configs/.local/lib/hyde/themeswitch.sh
@@ -89,7 +89,7 @@ source "${scrDir}/globalcontrol.sh"
 
 #// hypr
 if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
-    hyprctl keyword misc:disable_autoreload 1
+    hyprctl keyword misc:disable_autoreload 1 -q
 fi
 # shellcheck disable=SC2154
 sed '1d' "${HYDE_THEME_DIR}/hypr.theme" >"${confDir}/hypr/themes/theme.conf" # Useless and already handled by swwwallbash.sh but kept for robustness
