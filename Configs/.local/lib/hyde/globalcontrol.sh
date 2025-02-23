5a3b586aa997936ea66c36473f9e836ffae20afb
kRHYME7 <>
2025-02-23 10:33:54 +0800

    added hyde_share_dir as xdg_data_dirs

--- Configs/.local/lib/hyde/globalcontrol.sh
+++ Configs/.local/lib/hyde/globalcontrol.sh
@@ -7,6 +7,7 @@ export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+export XDG_DATA_DIRS="$XDG_DATA_HOME/hyde:/usr/local/share/hyde/:/usr/share/hyde/:$XDG_DATA_DIRS"
 
 # hyde envs
 export HYDE_CONFIG_HOME="${XDG_CONFIG_HOME}/hyde"
