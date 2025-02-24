2e292b14eebc0e4ca1036c7b250f80edd84d2207
kRHYME7 <>
2025-02-25 00:19:30 +0800

    fix variable expansion failed closes [Bug]: rofi launcher not showing apps and poweroptions also not visible emoji #296 close [Bug]: rofi launcher not showing apps and poweroptions also not visible emoji #296

--- Configs/.config/fish/config.fish
+++ Configs/.config/fish/config.fish
@@ -1,5 +1,7 @@
 set -g fish_greeting
 
+source ~/.config/fish/hyde_config.fish
+
 if status is-interactive
     starship init fish | source
 end
@@ -10,6 +12,7 @@ alias ls='eza -1   --icons=auto' # short list
 alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
 alias ld='eza -lhD --icons=auto' # long list dirs
 alias lt='eza --icons=auto --tree' # list folder as tree
+alias vc='code'
 
 # Handy change dir shortcuts
 abbr .. 'cd ..'
--- Configs/.zshenv
+++ Configs/.zshenv
@@ -135,6 +135,7 @@ function no_such_file_or_directory_handler {
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 XDG_CONFIG_DIR="${XDG_CONFIG_DIR:-HOME/.config}"
 XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+XDG_DATA_DIRS="${XDG_DATA_DIRS:-$XDG_DATA_HOME:/usr/local/share:/usr/share}"
 XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 XDG_DESKTOP_DIR="${XDG_DESKTOP_DIR:-$HOME/Desktop}"
