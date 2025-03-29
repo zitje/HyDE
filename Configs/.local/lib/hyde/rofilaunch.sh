253bfd13ff3a7043a1f61f49699c06a9e3a8e8d6
Jax Tsai <93467279+Jax-Tsai-zero@users.noreply.github.com>
2025-03-29 16:21:17 +0800

    Optimize rofilaunch script (#420)
    optimize rofilaunch

--- Configs/.local/lib/hyde/rofilaunch.sh
+++ Configs/.local/lib/hyde/rofilaunch.sh
@@ -53,11 +53,6 @@ h | --help)
     ;;
 esac
 
-# Run on uwsm if installed
-if pkg_installed uwsm; then
-    run_uwsm="uwsm app -- {cmd}"
-fi
-
 #// set overrides
 hypr_border="${hypr_border:-10}"
 hypr_width="${hypr_width:-2}"
@@ -96,7 +91,7 @@ rofi -show "${r_mode}" \
     -theme-str "${font_override}" \
     -theme-str "${i_override}" \
     -theme-str "${r_override}" \
-    -run-command "${run_uwsm:-"{cmd}"}" \
+    -run-command "sh -c 'uwsm app -- {cmd} || {cmd}'" \
     -theme "${rofi_config}" &
 disown
 
