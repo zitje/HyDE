4b7a9b7357c798df6a84dcbaedf635baf5559fcc
kRHYME7 <53417443+kRHYME7@users.noreply.github.com>
2025-05-17 11:01:06 +0800

    fix: [Bug]: Screen locking doesn't work until after a Hyprland reload #612
    close #612
    fix #612

--- Configs/.local/lib/hyde/lockscreen.sh
+++ Configs/.local/lib/hyde/lockscreen.sh
@@ -8,4 +8,4 @@ HYDE_RUNTIME_DIR="${HYDE_RUNTIME_DIR:-$XDG_RUNTIME_DIR/hyde}"
 # shellcheck disable=SC1091
 source "${HYDE_RUNTIME_DIR}/environment"
 
-"${LOCKSCREEN}" "${@}"
+"${LOCKSCREEN:-hyprlock}" "${@}"
