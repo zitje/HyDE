d3ba6a14199ec3eef3f38eade9d31d59c447b369
kRHYME7 <53417443+kRHYME7@users.noreply.github.com>
2025-06-06 09:11:56 +0800

    fix(zsh): Syntax error
    Close [Bug]: Zsh error on startup #741
    Close #741

--- Configs/.user.zsh
+++ Configs/.user.zsh
@@ -7,9 +7,10 @@ if [[ $- == *i* ]]; then
         pokego --no-title -r 1,3,6
     elif command -v pokemon-colorscripts >/dev/null; then
         pokemon-colorscripts --no-title -r 1,3,6
-elif command -v fastfetch >/dev/null; then
-    if do_render "image"; then
-        fastfetch --logo-type kitty
+    elif command -v fastfetch >/dev/null; then
+        if do_render "image"; then
+            fastfetch --logo-type kitty
+        fi
     fi
 fi
 # fastfetch.sh
