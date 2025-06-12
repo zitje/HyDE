20a30af272338c88561c9b494fd9323fc46f56a9
kRHYME7 <53417443+kRHYME7@users.noreply.github.com>
2025-06-12 13:21:15 +0800

    fix : [Bug]: Executing `resetxdgportal.sh` on startup may conflict with systemd services #727
    Resolve #727
    
    This is from the wiki

--- Configs/.local/lib/hyde/resetxdgportal.sh
+++ Configs/.local/lib/hyde/resetxdgportal.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 sleep 1
-killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-gnome
-killall xdg-desktop-portal-kde
-killall xdg-desktop-portal-lxqt
-killall xdg-desktop-portal-wlr
+killall -e xdg-desktop-portal-hyprland
 killall xdg-desktop-portal
 sleep 1
 
